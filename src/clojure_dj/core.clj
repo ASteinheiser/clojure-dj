412eca9bb9def7688c917fda79b70f6b6590c708
Andrew <me@iamandrew.io>
2019-04-23 00:46:48 -0700

    add chord progression

--- src/clojure_dj/core.clj
+++ src/clojure_dj/core.clj
@@ -2,25 +2,27 @@
   (:gen-class)
   (:use overtone.live))
 
-(defsynth fuzz-synth
-  "A fuzzing sound."
-  [freq 200 duration 1.5]
-  (let [src (- (sin-osc freq (* 2 Math/PI (lf-saw 2)))
-               (lf-saw freq))
-        env (env-gen (perc 0.1 duration) :action FREE)]
-    (out 0 (pan2 (* src env)))))
+(definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+  (* (env-gen (env-lin attack sustain release) 1 1 0 1 FREE)
+     (saw freq)
+     vol))
 
-(defn fuzz-beat
-  "Lifechanging beat."
-  [speed]
-  (while (< 0 1)
-    (do
-      (fuzz-synth)
-      (Thread/sleep (* 1000 speed))
-      (fuzz-synth)
-      (Thread/sleep (* 3000 speed)))))
+(defn saw2 [music-note]
+  (saw-wave (midi->hz (note music-note))))
+
+(defn play-chord [a-chord]
+  (doseq [note a-chord] (saw2 note)))
+
+(defn chord-progression-beat [m beat-num]
+  (at (m (+ 0 beat-num)) (play-chord (chord :C4 :major)))
+  (at (m (+ 4 beat-num)) (play-chord (chord :G3 :major)))
+  (at (m (+ 8 beat-num)) (play-chord (chord :A3 :minor)))
+  (at (m (+ 14 beat-num)) (play-chord (chord :F3 :major)))
+  (apply-at (m (+ 16 beat-num)) chord-progression-beat m (+ 16 beat-num) []))
+
+(defonce metro (metronome 120))
 
 (defn -main
   "Insert Beautiful Music Here."
   [& args]
-  (fuzz-beat 1))
+  (chord-progression-beat metro (metro)))
