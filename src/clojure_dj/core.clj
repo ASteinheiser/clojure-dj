8f0768a0d812916f61316774e704846abc7d486f
Andrew <me@iamandrew.io>
2019-04-23 20:34:25 -0700

    add kick sound and make drum beat

--- src/clojure_dj/core.clj
+++ src/clojure_dj/core.clj
@@ -13,6 +13,11 @@
 (defn play-chord [a-chord]
   (doseq [note a-chord] (saw2 note)))
 
+(definst kick [freq 50 env-ratio 3 freq-decay 0.02 amp-decay 0.5]
+  (let [fenv (* (env-gen (envelope [env-ratio 1] [freq-decay] :exp)) freq)
+        aenv (env-gen (perc 0.005 amp-decay) :action FREE)]
+    (* (sin-osc fenv (* 0.5 Math/PI)) aenv)))
+
 (defn chord-progression-beat [m beat-num]
   (at (m (+ 0 beat-num)) (play-chord (chord :C4 :major)))
   (at (m (+ 4 beat-num)) (play-chord (chord :G3 :major)))
@@ -20,9 +25,27 @@
   (at (m (+ 14 beat-num)) (play-chord (chord :F3 :major)))
   (apply-at (m (+ 16 beat-num)) chord-progression-beat m (+ 16 beat-num) []))
 
+(defn drum-beat [m beat-num]
+  (at (m (+ 1 beat-num)) (kick))
+  (at (m (+ 3 beat-num)) (kick))
+  (at (m (+ 5 beat-num)) (kick))
+  (at (m (+ 6 beat-num)) (kick))
+  (at (m (+ 6.5 beat-num)) (kick))
+  (at (m (+ 6.75 beat-num)) (kick))
+  (at (m (+ 7 beat-num)) (kick))
+  (at (m (+ 9 beat-num)) (kick))
+  (at (m (+ 10 beat-num)) (kick))
+  (at (m (+ 10.5 beat-num)) (kick))
+  (at (m (+ 10.75 beat-num)) (kick))
+  (at (m (+ 11 beat-num)) (kick))
+  (at (m (+ 13 beat-num)) (kick))
+  (at (m (+ 15 beat-num)) (kick))
+  (apply-at (m (+ 16 beat-num)) drum-beat m (+ 16 beat-num) []))
+
 (defonce metro (metronome 120))
 
 (defn -main
   "Insert Beautiful Music Here."
   [& args]
-  (chord-progression-beat metro (metro)))
+  ; (chord-progression-beat metro (metro))
+  (drum-beat metro (metro)))
