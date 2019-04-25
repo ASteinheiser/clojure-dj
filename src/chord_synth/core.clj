bf75a248469e66b9aee279befa6ab96857b29e37
Andrew <me@iamandrew.io>
2019-04-24 20:21:17 -0700

    lower chord volume

--- src/chord_synth/core.clj
+++ src/chord_synth/core.clj
@@ -2,7 +2,7 @@
   (:gen-class)
   (:use overtone.live))
 
-(definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+(definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.2] 
   (* (env-gen (env-lin attack sustain release) 1 1 0 1 FREE)
      (saw freq)
      vol))
